a0406984ddb682c34df6d4f5298863e9efd03de8
soo <sooooscode@gmail.com>
2025-03-17 04:02:29 +0900

    be 복구 완료

--- backend/src/main/java/com/modam/backend/ModamApplication.java
+++ backend/src/main/java/com/modam/backend/ModamApplication.java
@@ -2,12 +2,15 @@ package com.modam.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-public class BackendApplication {
+@ComponentScan(basePackages = "com.modam")
+public class ModamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(ModamApplication.class, args);
 	}
 
 }
