44448f074238a21036868be9d0015420990a3ca3
sooooscode <sooooscode@gmail.com>
2025-03-18 21:25:09 +0900

    add ChatController.java

---
+++ backend/src/main/java/com/modam/backend/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.modam.backend.controller;
+
+import com.modam.backend.dto.ChatMessageDto;
+import com.modam.backend.service.ChatService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/chat/{roomId}")
+    public ChatMessageDto sendMessage(ChatMessageDto message) {
+        return chatService.saveMessage(message);
+    }
+
+    @GetMapping("/history/{roomId}")
+    public List<ChatMessageDto> getChatHistory(@PathVariable String roomId) {
+        return chatService.getChatHistory(roomId);
+    }
+}
