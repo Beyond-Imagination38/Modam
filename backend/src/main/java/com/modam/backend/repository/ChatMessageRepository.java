7ccb89821ee0a0c616dd4ad544e65b7973f496e6
sooooscode <sooooscode@gmail.com>
2025-03-18 21:27:44 +0900

    add ChatMessageRepository.java

---
+++ backend/src/main/java/com/modam/backend/repository/ChatMessageRepository.java
@@ -0,0 +1,9 @@
+package com.modam.backend.repository;
+
+import com.modam.backend.model.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByRoomId(String roomId);
+}
