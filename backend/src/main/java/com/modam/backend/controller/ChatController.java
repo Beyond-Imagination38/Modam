e182e25437a3e8c378678111df420752cf72f295
sooooscode <sooooscode@gmail.com>
2025-04-10 00:03:24 +0900

    ChatController 수정

--- backend/src/main/java/com/modam/backend/controller/ChatController.java
+++ backend/src/main/java/com/modam/backend/controller/ChatController.java
@@ -3,8 +3,10 @@ package com.modam.backend.controller;
 import com.modam.backend.dto.ChatMessageDto;
 import com.modam.backend.service.ChatService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,11 +20,16 @@ public class ChatController {
         this.chat_service = chat_service;
     }
 
-    @MessageMapping("/chat/{club_id}")
-    @SendTo("/topic/chat/{club_id}")
-    public ChatMessageDto sendMessage(@PathVariable("club_id") int club_id, ChatMessageDto message) {
-        return chat_service.saveChatMessage(club_id, message);
+    @MessageMapping("/chat")  // 경로 단순화
+    public void sendMessage(ChatMessageDto message) {
+        // 클럽 ID는 메시지 내부에서 꺼냄
+        chat_service.saveChatMessage(message.getClubId(), message);
     }
+//    @MessageMapping("/chat")
+//    @SendTo("/topic/chat/{club_id}")
+//    public ChatMessageDto sendMessage(@PathVariable("club_id") int club_id, ChatMessageDto message) {
+//        return chat_service.saveChatMessage(club_id, message);
+//    }
 
     @GetMapping("/history/{club_id}")
     public List<ChatMessageDto> getChatHistory(@PathVariable("club_id") int club_id) {
