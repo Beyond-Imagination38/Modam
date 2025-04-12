6b6e6f45a612969acc6523207eaa72218f4dc828
sooooscode <sooooscode@gmail.com>
2025-04-12 14:51:04 +0900

    api 명세서 관련 수정

--- backend/src/main/java/com/modam/backend/dto/MemoDto.java
+++ backend/src/main/java/com/modam/backend/dto/MemoDto.java
@@ -1,5 +1,7 @@
 package com.modam.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,23 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class MemoDto {
 
+    @Schema(description = "메모 고유 ID", example = "901")
     private Long memo_id;
+
+    @Schema(description = "북클럽 ID", example = "9")
     private int club_id;
+
+    @Schema(description = "작성자 유저 ID", example = "user1")
     private String user_id;
+
+    @Schema(description = "메모 내용", example = "모임 중 핵심 토픽 정리 메모입니다.")
     private String content;
+
+    //추후 수정
+    @Schema(description = "메모 작성 시간", example = "2025-04-10T16:20:00")
     private LocalDateTime created_time;
+
+    //추후 수정
+    @Schema(description = "메모 수정 시간", example = "2025-04-10T16:25:00")
     private LocalDateTime updated_time;
 }
--- backend/src/main/java/com/modam/backend/dto/UserDto.java
+++ backend/src/main/java/com/modam/backend/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.modam.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,21 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserDto {
 
+    @Schema(description = "유저 고유 ID", example = "1")
     private int user_id;
+
+    @Schema(description = "유저 이름", example = "사용자01")
     private String user_name;
+
+    @Schema(description = "유저 이메일", example = "user01@example.com")
     private String email;
+
+    @Schema(description = "비밀번호", example = "pw01")
     private String pw;
+
+    @Schema(description = "프로필 이미지 URL (없을 경우 NULL)", example = "NULL")
     private String profile_image;
+
+    @Schema(description = "보유 코인", example = "0")
     private int coin;
 }
