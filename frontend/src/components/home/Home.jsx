7868bbbdaacb05f6a43bd33954e9b91a2825cf49
seo <yellow917330@gmail.com>
2025-04-15 14:09:56 +0900

    home 로고 이미지  추가

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import * as S from "./Home.style";
-import logo from "./logo.png";
 
 export function Home() {
   return (
     <S.HomeContainer>
-      <S.Title><img src={logo} alt="Logo"/></S.Title>
+      <S.Title>
+        <img src="/assets/logo.png" alt="Logo" />
+      </S.Title>
       <S.Nav>
         <Link to="/signup">
           <S.SignupButton>회원가입</S.SignupButton>
@@ -13,7 +14,6 @@ export function Home() {
         <Link to="/login">
           <S.LoginButton>로그인</S.LoginButton>
         </Link>
-
         <Link to="/main">
           <S.GoButton>일단 둘러보기</S.GoButton>
         </Link>
