48277add12c5791320891dcffa19e47fc7b1affd
sooooscode <sooooscode@gmail.com>
2025-05-21 23:34:07 +0900

    memo 연결 완료

--- backend/src/main/java/com/modam/backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/modam/backend/security/JwtAuthenticationFilter.java
@@ -27,6 +27,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        System.out.println("JwtAuthenticationFilter doFilterInternal called for URL: " + request.getRequestURI());//디버그
+
+
         String header = request.getHeader("Authorization");
         if (header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
