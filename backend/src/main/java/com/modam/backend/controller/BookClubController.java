3d0df05536014ba8eab1d22f6615e05a7fe65fd8
sooooscode <sooooscode@gmail.com>
2025-05-25 22:37:27 +0900

    북클럽 상세 페이지 조회 API 추가

--- backend/src/main/java/com/modam/backend/controller/BookClubController.java
+++ backend/src/main/java/com/modam/backend/controller/BookClubController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class BookClubController {
     @GetMapping
     public ResponseEntity<List<ClubListDto>> getAllClubs() {
         return ResponseEntity.ok(bookClubService.getAllClubSummaries());
+    }
+
+    @GetMapping("/{clubId}")
+    public ResponseEntity<ClubListDto> getBookClubById(@PathVariable int clubId){
+        return ResponseEntity.ok(bookClubService.getClubSummaryById(clubId));
     }
 }
