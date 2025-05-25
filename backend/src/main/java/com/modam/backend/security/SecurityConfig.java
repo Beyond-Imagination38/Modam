e1af29cbb6420855b6f7752b3b62563173870602
sooooscode <sooooscode@gmail.com>
2025-05-25 22:08:31 +0900

    cors 설정 permitAll 추가

--- backend/src/main/java/com/modam/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/modam/backend/security/SecurityConfig.java
@@ -1,18 +1,19 @@
 package com.modam.backend.security;
+
 import com.modam.backend.util.JwtUtil;
-
-
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -37,6 +38,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())  // REST API: 보통 CSRF 비활성화
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // cors 설정 추가
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // <-- 이거 추가!!
                         .requestMatchers(
                                 "/swagger-ui/**",              // soo: swagger 예외처리
                                 "/v3/api-docs/**",             // soo: swagger 예외처리
@@ -72,5 +74,20 @@ public class SecurityConfig {
         return source;
     }
 
+    //우선적으로 적용되도록 설정
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0); // 가장 먼저 적용되도록 우선순위 설정
+        return bean;
+    }
 
 }
