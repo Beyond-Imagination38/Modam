a2e1fa83542e81ab7970da27610a6b6fb5e52a82
sooooscode <sooooscode@gmail.com>
2025-05-25 21:15:47 +0900

    cors 설정 추가 완료 - 오ëpreflight cors ¥ 해결

--- backend/src/main/java/com/modam/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/modam/backend/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.modam.backend.security;
-
 import com.modam.backend.util.JwtUtil;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,13 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
 
 @Configuration
 public class SecurityConfig {
@@ -26,7 +34,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // REST API는 보통 CSRF 비활성화
+                .csrf(csrf -> csrf.disable())  // REST API: 보통 CSRF 비활성화
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // cors 설정 추가
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",              // soo: swagger 예외처리
@@ -48,4 +57,20 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    //cors 설정 추가
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000")); // FE 주소
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // 쿠키 인증 등 사용시 true
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+
 }
