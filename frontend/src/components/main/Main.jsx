a8477b0654c1b6ee25cf16f3c6ee9e40125bacde
seo <yellow917330@gmail.com>
2025-06-03 11:57:28 +0900

    main api 호출 수정

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -5,101 +5,17 @@ import { useState, useEffect } from "react";
 import { API_URLS } from "../../consts";
 import { fetchApi } from "../../utils";
 
-const ITEMS_PER_PAGE = 3;
-
 export function Main() {
   const [searchTerm, setSearchTerm] = useState("");
   const [items, setItems] = useState([]);
   const [activeCategory, setActiveCategory] = useState("진행 중");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [user, setUser] = useState(null); 
 
   const navigate = useNavigate();
 
   const toggleSidebar = () => {
     setIsSidebarOpen((prev) => !prev);
   };
-  /*
-  const data = [
-    {
-      postId: 1,
-      userId: 20,
-      title: "1984",
-      time: "2025-04-10 20:00",
-      representativeImage: "/assets/images/1984.jpg",
-      category: "완료",
-    },
-    {
-      postId: 2,
-      userId: 20,
-      title: "자아폭발",
-      time: "2025-04-13 16:00",
-      representativeImage: "/assets/images/TheFall.jpg",
-      category: "좋아요",
-    },
-    {
-      postId: 3,
-      title: "참을 수 없는 존재의 가벼움",
-      time: "2025-04-11 08:00",
-      representativeImage: "/assets/images/Unbearable.jpg",
-      category: "진행 중",
-    },
-    {
-      postId: 4,
-      title: "앵무새 죽이기",
-      time: "2025-04-11 10:00",
-      representativeImage: "/assets/images/mockingbird.jpg",
-      category: "좋아요",
-    },
-    {
-      postId: 5,
-      title: "데미안",
-      time: "2025-04-16 08:00",
-      representativeImage: "/assets/images/demian.jpg",
-      category: "완료",
-    },
-    {
-      postId: 6,
-      title: "죽음의 수용소에서",
-      time: "2025-04-15 08:00",
-      representativeImage: "/assets/images/searchfor.jpg",
-      category: "진행 중",
-    },
-    {
-      postId: 7,
-      title: "싯다르타",
-      time: "2025-04-15 04:00",
-      representativeImage: "/assets/images/Sidd.jpg",
-      category: "완료",
-    },
-    {
-      postId: 8,
-      title: "소크라테스 익스프레스",
-      time: "2025-04-11 06:00",
-      representativeImage: "/assets/images/socra.jpg",
-      category: "좋아요",
-    },
-    {
-      postId: 9,
-      title: "구의 증명",
-      time: "2025-04-11 08:00",
-      representativeImage: "/assets/images/goo.jpg",
-      category: "진행 중",
-    },
-  ];
-  
-
-  useEffect(() => {
-    const storedPosts = JSON.parse(localStorage.getItem("posts")) || [];
-
-    const combined = [...storedPosts, ...data];
-    const unique = Array.from(
-      new Map(combined.map((item) => [item.postId || item.title, item])).values()
-    );
-
-    setItems(unique);
-  }, [activeCategory, searchTerm, currentPage]);
-  */
 
   useEffect(() => {
     const fetchOngoingClubs = async () => {
@@ -112,18 +28,18 @@ export function Main() {
           return;
         }
 
-        const response = await fetch(`http://localhost:8080/api/bookclubs/my/ongoing?userId=${userId}`, {
+        const response = await fetch(`http://localhost:8080/api/bookclubs`, {
           method: "GET",
         });
 
         if (!response.ok) {
-          throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+          throw new Error(`서버 응답 실패: ${response.status}`);
         }
 
         const data = await response.json();
 
         const mapped = data.map((item, index) => ({
-          postId: item.clubId, // 임시
+          postId: item.clubId, 
           title: item.bookTitle,
           time: item.meetingDatTime,
           representativeImage: item.coverImage,
