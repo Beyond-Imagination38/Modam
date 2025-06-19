8b948642daf7f60561d4c7a061c6da90bba4f3e7
seo <yellow917330@gmail.com>
2025-06-19 22:32:40 +0900

    fix: API 주소 프록시 구조 수정 및 consts 정리

--- frontend/src/consts/index.js
+++ frontend/src/consts/index.js
@@ -1,21 +1,44 @@
-export const COMMON_API_URL = "/api";
+const COMMON_API_URL = "/api";       
+const EXTERNAL_API_URL = "/external";
 
-export const API_URLS = {
-  user: `${COMMON_API_URL}/user`,
-  signup: `${COMMON_API_URL}/user/signup`,
-  chat: `${COMMON_API_URL}/chat`,
-  mypage: `${COMMON_API_URL}/mypage`,
+const API_URLS = {
   bookreport: `${COMMON_API_URL}/reading-notes`,
-  bookclubs: `${COMMON_API_URL}/api/bookclubs`,
-  myOngoing: (userId) => `${COMMON_API_URL}/api/bookclubs/my/ongoing?userId=${userId}`,
-  myCompleted: (userId) => `${COMMON_API_URL}/api/bookclubs/my/completed?userId=${userId}`,
-  allBookclubs: `${COMMON_API_URL}/api/bookclubs/search?sortBy=latest`,
-  memo: (clubId, userId) => `${COMMON_API_URL}/api/memo/${clubId}/${userId}`,
-  finalizeMemo: (clubId, userId) => `${COMMON_API_URL}/api/memo/${clubId}/${userId}/finalize`,
-  bookclubDetail: (clubId, userId) => `${COMMON_API_URL}/api/bookclubs/${clubId}/detail?userId=${userId}`,
-  joinBookclub: (clubId, userId) => `${COMMON_API_URL}/api/bookclubs/${clubId}/join?userId=${userId}`,
-  completedDetail: (clubId) => `${COMMON_API_URL}/api/bookclubs/${clubId}/completed-detail`,
-  getUserInfo: (userId) => `${COMMON_API_URL}/user/${userId}`,
-  updateUserName: (userId) => `${COMMON_API_URL}/user/${userId}/name`,
-  updateUserPassword: (userId) => `${COMMON_API_URL}/user/${userId}/password`,
+
+  signup: `${EXTERNAL_API_URL}/user/signup`,
+  user: `${EXTERNAL_API_URL}/user`,
+  chat: `${EXTERNAL_API_URL}/chat`,
+  mypage: `${EXTERNAL_API_URL}/mypage`,
+
+  bookclubs: `${COMMON_API_URL}/bookclubs`,
+  myOngoing: function (userId) {
+    return `${COMMON_API_URL}/bookclubs/my/ongoing?userId=${userId}`;
+  },
+  myCompleted: function (userId) {
+    return `${COMMON_API_URL}/bookclubs/my/completed?userId=${userId}`;
+  },
+  allBookclubs: `${COMMON_API_URL}/bookclubs/search?sortBy=latest`,
+  memo: function (clubId, userId) {
+    return `${COMMON_API_URL}/memo/${clubId}/${userId}`;
+  },
+  finalizeMemo: function (clubId, userId) {
+    return `${COMMON_API_URL}/memo/${clubId}/${userId}/finalize`;
+  },
+  bookclubDetail: function (clubId, userId) {
+    return `${COMMON_API_URL}/bookclubs/${clubId}/detail?userId=${userId}`;
+  },
+  joinBookclub: function (clubId, userId) {
+    return `${COMMON_API_URL}/bookclubs/${clubId}/join?userId=${userId}`;
+  },
+  completedDetail: function (clubId) {
+    return `${COMMON_API_URL}/bookclubs/${clubId}/completed-detail`;
+  },
+  getUserInfo: function (userId) {
+    return `${EXTERNAL_API_URL}/user/${userId}`;
+  },
+  updateUserName: function (userId) {
+    return `${EXTERNAL_API_URL}/user/${userId}/name`;
+  },
+  updateUserPassword: function (userId) {
+    return `${EXTERNAL_API_URL}/user/${userId}/password`;
+  }
 };
